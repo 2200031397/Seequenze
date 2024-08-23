4d57305cbbeb42f3d03355122a9f576e661752a9
2200031397 <139319227+2200031397@users.noreply.github.com>
2024-08-23 22:12:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,10 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const Router = require('./route/router');
 
-const mongoose = require('mongoose');
-
 const dburl = 'mongodb+srv://charanchowdarynunnam:KzULXYPA8UqWl4R4@seequenze.ngp9l.mongodb.net/?retryWrites=true&w=majority&appName=Seequenze';
-
-mongoose.connect(dburl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000 // Increase timeout to 30 seconds
-})
-.then(() => console.log('Connected to DB Successfully!'))
-.catch((err) => console.log('Database connection error:', err.message));
-
+mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to DB Successfully!'))
+  .catch((err) => console.log('Database connection error:', err.message));
 
 const app = express();
 app.use(cors());
